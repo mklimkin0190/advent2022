863ae7b8773620758700ccf4bf905d3754498c78
Masha Klimkin <mklimkina@gmail.com>
2023-06-01 23:18:35 +0200

    more code reuse

--- src/solutions/day-7/1.ts
+++ src/solutions/day-7/1.ts
@@ -1,42 +1,4 @@
-import { Node, newNode } from './shared.ts'
-
-export const buildFilesTree = (output: string[], node: Node, sum = 0) => {
-  if (output.length === 0) {
-    if (node?.name) {
-      // need to go back to the top to count all sizes
-      return buildFilesTree(['$ cd ..'], node, sum)
-    }
-    return sum
-  }
-  const [line, ...rest] = output
-  const [first, second, third] = line.split(' ')
-  if (first === '$') {
-    // command
-    if (second === 'cd') {
-      if (third === '..') {
-        // go up
-        node.size = node.children.reduce((sum: number, curr: Node) => sum + curr.size, 0)
-        if (node.size <= 100000) {
-          return buildFilesTree(rest, node.parent, sum + node.size)
-        }
-        return buildFilesTree(rest, node.parent, sum)
-      } else {
-        // go in
-        const child = node.children.find((ch: Node) => ch.name === third)
-        return buildFilesTree(rest, child, sum)
-      }
-    } else if (second === 'ls') {
-      // prepare to add children
-      node.children = []
-      return buildFilesTree(rest, node, sum)
-    }
-  } else {
-    // add child
-    const childSize = (first === 'dir' ? 0 : +first)
-    node.children = [...node.children, newNode(second, node, childSize)]
-    return buildFilesTree(rest, node, sum)
-  }
-}
+import { Node, newNode, buildFilesTree } from './shared.ts'
 
 export const run = (input: string) => {
   const lines = input.split('\n')
@@ -46,7 +8,13 @@ export const run = (input: string) => {
     children: [newNode('/', this, 0)],
     parent: null
   }
-  const res = buildFilesTree(lines, filesTree, 0)
-  return res
+  const res = buildFilesTree(lines, filesTree, [])
+  const sorted = res.sort((a, b) => b - a)
+  return res.reduce((sum: number, curr: number) => {
+    if (curr < 100000) {
+      return sum + curr
+    }
+    return sum
+  }, 0)
 }
 
--- src/solutions/day-7/shared.ts
+++ src/solutions/day-7/shared.ts
@@ -12,3 +12,38 @@ export const newNode = (name: string, parent: Node | null = null, size: number) 
   parent,
 })
 
+export const buildFilesTree = (output: string[], node: Node, acc = []) => {
+  if (output.length === 0) {
+    if (node?.name) {
+      // need to go back to the top to count all sizes
+      return buildFilesTree(['$ cd ..'], node, acc)
+    }
+    return acc
+  }
+  const [line, ...rest] = output
+  const [first, second, third] = line.split(' ')
+  if (first === '$') {
+    // command
+    if (second === 'cd') {
+      if (third === '..') {
+        // go up
+        node.size = node.children.reduce((sum: number, curr: Node) => sum + curr.size, 0)
+        return buildFilesTree(rest, node.parent, [...acc, node.size])
+      } else {
+        // go in
+        const child = node.children.find((ch: Node) => ch.name === third)
+        return buildFilesTree(rest, child, acc)
+      }
+    } else if (second === 'ls') {
+      // prepare to add children
+      node.children = []
+      return buildFilesTree(rest, node, acc)
+    }
+  } else {
+    // add child
+    const childSize = (first === 'dir' ? 0 : +first)
+    node.children = [...node.children, newNode(second, node, childSize)]
+    return buildFilesTree(rest, node, acc)
+  }
+}
+
